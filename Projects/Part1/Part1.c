d901e5f288cb4b3964d04f7464aad9822c33e78e
karterk <karterk@iastate.edu>
2019-09-10 14:33:19 -0500

    Restructure folders/files

---
+++ Projects/Part1/Part1.c
@@ -0,0 +1,4 @@
+//
+// Created by Karter Krueger on 9/10/19.
+//
+
--- hw/hw3/hw3.c
+++ hw/hw3/hw3.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void initialize(char digits[], unsigned numdigits, unsigned value);
 int add(char result[], char num1[], char num2[], unsigned numdigits);
 void print_int(char digits[], unsigned numdigits);
 
 int main() {
-	char *digits[];
-	initialize(&digits, 10, 0);
+	char* digits;
+	initialize(digits, 10, 0);
 	int i;
 	for (i = 0; i < 10; i++) {
 		printf(digits[i]);
@@ -16,8 +17,8 @@ int main() {
 void initialize(char digits[], unsigned numdigits, unsigned value) {
 	int i;
 	for (i = 0; i < numdigits; i++) {
-		*digits[i] = malloc(sizeof(char));
-		digits[i] = '';	
+		digits[i] = malloc(sizeof(char));
+		digits[i] = ' ';
 	}
 }
 
