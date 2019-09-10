8d09a63abb69ed703e0f3f8a0a6feb86bb1c9f79
Karter Krueger <krueger.karter@gmail.com>
2019-09-10 14:11:11 -0500

    Started hw3 and add .gitignore

---
+++ HW3/hw3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void initialize(char digits[], unsigned numdigits, unsigned value);
+int add(char result[], char num1[], char num2[], unsigned numdigits);
+void print_int(char digits[], unsigned numdigits);
+
+int main() {
+	char *digits[];
+	initialize(&digits, 10, 0);
+	int i;
+	for (i = 0; i < 10; i++) {
+		printf(digits[i]);
+	}
+}
+
+void initialize(char digits[], unsigned numdigits, unsigned value) {
+	int i;
+	for (i = 0; i < numdigits; i++) {
+		*digits[i] = malloc(sizeof(char));
+		digits[i] = '';	
+	}
+}
+
+int add(char result[], char num1[], char num2[], unsigned numdigits) {
+
+
+}
+
+void print_int(char digits[], unsigned numdigits) {
+
+}
+ 
