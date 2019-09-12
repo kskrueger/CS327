6837dc19e6d22560b3ea088f97fd2bf5b7e0ced4
Karter Krueger <krueger.karter@gmail.com>
2019-09-12 15:11:40 -0500

    HW3 working

--- hw/hw3/hw3.c
+++ hw/hw3/hw3.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <math.h>
 #define DEBUG 0
 
 void initialize(char digits[], unsigned numdigits, unsigned value);
@@ -6,28 +7,40 @@ int add(char result[], char num1[], char num2[], unsigned numdigits);
 void print_int(char digits[], unsigned numdigits);
 
 int main() {
-	int size = 4;
-	char result[size];
-	char in1[size];
-	char in2[size];
-	initialize(in1, size, 9998);
-	initialize(in2, 1, 1);
-	if (add(result, in1, in2, size) != 0) {
-	    printf("Overflow!\n");
-	}
-	print_int(result, size);
+	int size = 50;
+	char n1[size];
+	char n2[size];
+	char n3[size];
+	initialize(n1, size, 0);
+    initialize(n2, size, 1);
+    printf("F(   0): %d\n", 0);
+    printf("F(   1): %d\n", 1);
+    int i = 2;
+    while (1) {
+        if (add(n3, n1, n2, size)) break;
+        printf("F(%4d): ", i++);
+        print_int(n3, size);
+        if (add(n1, n2, n3, size)) break;
+        printf("F(%4d): ", i++);
+        print_int(n1, size);
+        if (add(n2, n3, n1, size)) break;
+        printf("F(%4d): ", i++);
+        print_int(n2, size);
+    }
 	return 0;
 }
 
 void initialize(char digits[], unsigned numdigits, unsigned value) {
-	sprintf(digits, "%d", value);
-
+    int i;
+    for (i = 0; i < numdigits; i++) {
+        digits[i] = '0' + (int) (((int)value/ (int)pow(10, (numdigits-1-i)))%10);
+    }
 }
 
 int add(char result[], char num1[], char num2[], unsigned numdigits) {
 	int i;
 	int carry = 0;
-	printf("num1: %s, num2: %s\n", num1, num2);
+	// printf("num1: %s, num2: %s\n", num1, num2);
 	for(i = numdigits - 1; i >= 0; i--) {
 		int number = num1[i] - '0' + num2[i] - '0' + carry;
 		// printf("%d + %d + %d = %d\n", num1[i] - '0', num2[i] - '0', carry, number);
@@ -48,10 +61,14 @@ int add(char result[], char num1[], char num2[], unsigned numdigits) {
 
 void print_int(char digits[], unsigned numdigits) {
 	int i;
+	int zeros = 1;
 	for (i = 0; i < numdigits; i++) {
-	    printf("%c", digits[i]);
-	    if ((numdigits-1-i) % 3 == 0 && i != numdigits-1) {
-	        printf(",");
+	    if (digits[i] != '0') zeros = 0;
+	    if (!zeros) {
+            printf("%c", digits[i]);
+            if ((numdigits-1-i) % 3 == 0 && i != numdigits-1) {
+                printf(",");
+            }
 	    }
 	}
 	printf("\n");
