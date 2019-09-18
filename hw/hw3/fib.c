28255caf5d268fea698e585210673a6ddb467f10
Karter Krueger <>
2019-09-17 23:25:20 -0500

    Fixed hw3 fib.c without math lib

--- hw/hw3/fib.c
+++ hw/hw3/fib.c
@@ -1,17 +1,19 @@
-// HW3 CREATED BY KARTER KRUEGER on 9/10/19
-#include<stdio.h>
-#include <math.h>
+//
+// Created by Karter Krueger on 9/10/19.
+//
+#include <stdio.h>
+#include <string.h>
 
 void initialize(char digits[], unsigned numdigits, unsigned value);
 int add(char result[], char num1[], char num2[], unsigned numdigits);
 void print_int(char digits[], unsigned numdigits);
 
 int main() {
-	int size = 50; // number of digits for the large integers
-	char n1[size]; // large integer 1
-	char n2[size]; // large integer 2
-	char n3[size]; // large integer 3
-	initialize(n1, size, 0); // load 0 into integer 1
+    int size = 50; // number of digits for the large integers
+    char n1[size]; // large integer 1
+    char n2[size]; // large integer 2
+    char n3[size]; // large integer 3
+    initialize(n1, size, 0); // load 0 into integer 1
     initialize(n2, size, 1); // load 1 into integer 2
     printf("F(   0): %d\n", 0); // print first term
     printf("F(   1): %d\n", 1); // print second term
@@ -29,7 +31,7 @@ int main() {
         printf("F(%4d): ", i++);
         print_int(n2, size);
     }
-	return 0;
+    return 0;
 }
 
 int frf(int a, int b) {
@@ -59,41 +61,40 @@ void initialize(char digits[], unsigned numdigits, unsigned value) {
 
 // add two input numbers (in char arrays) into an output (char array)
 int add(char result[], char num1[], char num2[], unsigned numdigits) {
-	int i;
-	int carry = 0;
-	// add the numbers into the result array, one digit at a time, and carry to the next digit when needed
-	for(i = numdigits - 1; i >= 0; i--) {
-	    // subtract the '0' char value to get the number value
-		int number = num1[i] - '0' + num2[i] - '0' + carry;
-		if (number > 9) {
-			carry = 1;
-			number -= 10;
-		} else {
+    int i;
+    int carry = 0;
+    // add the numbers into the result array, one digit at a time, and carry to the next digit when needed
+    for(i = numdigits - 1; i >= 0; i--) {
+        // subtract the '0' char value to get the number value
+        int number = num1[i] - '0' + num2[i] - '0' + carry;
+        if (number > 9) {
+            carry = 1;
+            number -= 10;
+        } else {
             carry = 0;
         }
-		// add the '0' char value to get the correct char representation
-		result[i] = (number + '0');
-		if (i == 0 && carry == 1) {
+        // add the '0' char value to get the correct char representation
+        result[i] = (number + '0');
+        if (i == 0 && carry == 1) {
             return 1;
-		}
-	}
-	return 0;
+        }
+    }
+    return 0;
 }
 
 // print the large integer one digit at a time to the console and add , in every 3 places
 void print_int(char digits[], unsigned numdigits) {
-	int i;
-	int zeros = 1;
-	for (i = 0; i < numdigits; i++) {
-	    if (digits[i] != '0') zeros = 0;
-	    if (!zeros) {
+    int i;
+    int zeros = 1;
+    for (i = 0; i < numdigits; i++) {
+        if (digits[i] != '0') zeros = 0;
+        if (!zeros) {
             printf("%c", digits[i]);
             // check mod 3 to add a , every 3
             if ((numdigits-1-i) % 3 == 0 && i != numdigits-1) {
                 printf(",");
             }
-	    }
-	}
-	printf("\n");
+        }
+    }
+    printf("\n");
 }
- 
