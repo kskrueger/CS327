d424bfc21cb90108492784bd0b0e4a224365678d
Karter Krueger <karterk@iastate.edu>
2019-09-26 02:40:02 -0500

    Part1 Submission (Updated)

--- Projects/Part1/Part1.c
+++ Projects/Part1/Part1.c
@@ -20,7 +20,7 @@ const int ROWS = 8;
 int main() {
     char board[8][8]; // initialize the board array
     int red_kings = 0, red_pawns = 0, black_kings = 0, black_pawns = 0; // initialize variables
-    int capture_on = -1, multiple_jumps_on = -1, turn_red = -1, line_num = 1;
+    int capture_on = -1, multiple_jumps_on = -1, turn_red = -1, line_num = 1, errorOut = 0;
 
     // scan for each keyword and print error if it fails
     if (!(search("RULES:", &line_num))) error("RULES:", &line_num);
@@ -29,8 +29,10 @@ int main() {
     if (!(search("TURN:", &line_num))) error("TURN:", &line_num);
     if (!(search2("black", "red", &line_num, &turn_red))) error("red' or 'black", &line_num);
     if (!(search("BOARD:", &line_num))) error("BOARD:", &line_num);
-    if (!(scan_board(8,8, board, &line_num))) {
-        fprintf(stderr, "ERROR near line %d: board input/scan error", line_num);
+    if ((errorOut = scan_board(8,8, board, &line_num))) {
+        if (errorOut == -1) fprintf(stderr, "ERROR near line %d: unexpected end of file", line_num);
+        else if (errorOut == 1) fprintf(stderr, "ERROR near line %d: board error, pattern invalid", line_num);
+        else if (errorOut == 2) fprintf(stderr, "ERROR near line %d: board input error, invalid character", line_num);
         exit(1);
     }
 
@@ -52,7 +54,7 @@ void error(char *key, const int *line_num) {
 void skip_whitespace(int *line_num) {
     char c;
     int comment = 0;
-    while (fscanf(stdin, "%c", &c) && c!= EOF && (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '#' || comment)) {
+    while (fscanf(stdin, "%c", &c) == 1 && c!= EOF && (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '#' || comment)) {
         if (c == '\n') (*line_num)++; // increment line number
         if (comment) comment = (c != '\n'); // look for end of comment
         else comment = (c == '#'); // look for start of comment
@@ -66,7 +68,7 @@ int search(char *word, int *line_num) {
     char c;
     int i = 0;
     // loop through to search for word and increment line number for each \n character
-    while (fscanf(stdin, "%c", &c) && c!= EOF && (c == word[i++])) if (c == '\n') (*line_num)++;
+    while (fscanf(stdin, "%c", &c) == 1 && c!= EOF && (c == word[i++])) if (c == '\n') (*line_num)++;
     ungetc(c, stdin); // put the last char back so next function can use it
     return i-1 == strlen(word); // return 1 if word was found
 }
@@ -77,11 +79,11 @@ int search2(char *word0, char* word1, int *line_num, int *word) {
     char c;
     // select word 0 or 1 based on if the first letter matches (good enough for this project)
     int i = 0;
-    *word = (fscanf(stdin, "%c", &c) && c == word1[0]);
+    *word = (fscanf(stdin, "%c", &c) == 1 && c == word1[0]);
     ungetc(c, stdin);
     // loop through to search for word and increment line number for each \n character
-    while (!(*word) && fscanf(stdin, "%c", &c) && c!= EOF && (c == word0[i++])) if (c == '\n') (*line_num)++;
-    while ((*word) && fscanf(stdin, "%c", &c) && c!= EOF && (c == word1[i++])) if (c == '\n') (*line_num)++;
+    while (!(*word) && fscanf(stdin, "%c", &c) == 1 && c!= EOF && (c == word0[i++])) if (c == '\n') (*line_num)++;
+    while ((*word) && fscanf(stdin, "%c", &c) == 1 && c!= EOF && (c == word1[i++])) if (c == '\n') (*line_num)++;
     ungetc(c, stdin); // put the last char back so next function can use it
     return (i-1 == (!(*word) ? strlen(word0):strlen(word1))); // return 1 or 2 if word was found
 }
@@ -91,7 +93,7 @@ int scan_board(int rows, int cols, char array[rows][cols], int *line_num) {
     int row = 0, col = 0;
     char c;
     skip_whitespace(line_num); // skip whitespace to start
-    fscanf(stdin, "%c", &c);
+    if (fscanf(stdin, "%c", &c) != 1) return -1; // return -1 if not reading more input
     int black_square = (c == '\"'); // check if top left corner is black or red for FLIPPED board
     for (row = 0; row < rows && c != EOF; row++, black_square = !black_square) {  // invert black_square each loop
         for (col = 0; col < cols && c != EOF;) {
@@ -99,15 +101,15 @@ int scan_board(int rows, int cols, char array[rows][cols], int *line_num) {
             if (c == '\"' || c == 'b' || c == 'B' || c == 'r' || c == 'R'|| c == '.') {
                 array[row][col++] = c; // put value into array
                 if (!(black_square = !black_square)) {
-                    if (c != '\"') return 0; // exit if fault in pattern
-                } else if (c != 'r' && c != 'R' && c != 'b' && c != 'B' && c != '.') return 0; // exit if pattern fault
+                    if (c != '\"') return 1; // exit if fault in pattern
+                } else if (c != 'r' && c != 'R' && c != 'b' && c != 'B' && c != '.') return 1; // exit if pattern fault
             }
-            fscanf(stdin, "%c", &c);
+            if (fscanf(stdin, "%c", &c) != 1) return -1; // return -1 if not reading more input
             if (c != '\"' && c != 'b' && c != 'B' && c != 'r' && c != 'R'&& c != '.'
-                && c != ' ' && c != '\n' && c != '+' && c != '|' && c != '-') return 0; // exit if invalid character
+                && c != ' ' && c != '\n' && c != '+' && c != '|' && c != '-') return 2; // exit if invalid character
         }
     }
-    return 1; // return 1 for good if it reaches this point
+    return 0; // return 0 for good if it reaches this point
 }
 
 // count the pieces
