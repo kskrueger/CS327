890229edd5b4f0ae4491fe1a5a96998fe3998459
Karter Krueger <karterk@iastate.edu>
2019-10-01 23:38:00 -0500

    Comments added to HW4

--- hw/hw4/pascal.c
+++ hw/hw4/pascal.c
@@ -23,38 +23,37 @@ int main() {
 
 // function to build the first N rows of Pascal's triangle, where N is given as a parameter.
 unsigned long** build_Pascal(unsigned N) {
+    // initialize out with a chunk of memory for pointers to arrays of unsigned long
     unsigned long** out = malloc(N * sizeof(unsigned long*));
-    unsigned i, j;
-    for (i = 0; i < N; i++) {
-        out[i] = malloc((i + 1) * sizeof(unsigned long));
-        for (j = 0; j <= i; j++) {
-            if (j == 0 || j == i || i < 1) out[i][j] = 1;
-            else out[i][j] = out[i-1][j-1] + out[i-1][j];
+    unsigned i, j; // counters for loops
+    for (i = 0; i < N; i++) { // loop through for each line of the pascal triangle
+        out[i] = malloc((i + 1) * sizeof(unsigned long)); // reserve memory for each row of the triangle
+        for (j = 0; j <= i; j++) { // fill each value in each row
+            if (j == 0 || j == i || i < 1) out[i][j] = 1; // if it's the first row, or start/end column, set to 1
+            else out[i][j] = out[i-1][j-1] + out[i-1][j]; // otherwise, set the value to the sum of those above
         }
     }
-    return out;
+    return out; // return the pointer (that points to an array of pointers)
 }
 
 // function to display the first N rows of Pascal's triangle, where N and the triangle are given as parameters
 void show_Pascal(unsigned long** P, unsigned N) {
-    unsigned w, i, j;
-    int *widths = calloc(N, sizeof(int));
-    for (w = 0; w < N; w++) {
-        unsigned long num = P[N-1][w];
-        while ((num /= 10) > 9) widths[w]++;
+    unsigned w, i, j; // counters for loops
+    int *widths = calloc(N, sizeof(int)); // initialize an array of widths (of the columns) to all 0s in a size of N
+    for (w = 0; w < N; w++) { // for each column
+        unsigned long num = P[N-1][w]; // get the largest value, from the last row of the triangle
+        while ((num /= 10) > 9) widths[w]++; // determine the number of digits in the numbers, and increment in widths
     }
-    for (i = 0; i < N; i++) {
-        for (j = 0; j <= i; j++) {
-            fprintf(stdout, "%*lu ", widths[j]+2, P[i][j]);
+    for (i = 0; i < N; i++) { // loop for each row/line of triangle
+        for (j = 0; j <= i; j++) { // loop for each value in each row
+            fprintf(stdout, "%*lu ", widths[j]+2, P[i][j]); // print each number, with the correct width
         }
-        fprintf(stdout, "\n");
+        fprintf(stdout, "\n"); // new line after each row
     }
 }
 
 // function to destroy (free the allocated memory for) the first N rows of Pascal's triangle
 void destroy_Pascal(unsigned long** P, unsigned N) {
-    unsigned i = 0;
-    while (i < N) free(P[i++]);
-    free(P);
-    *P = NULL;
+    unsigned i = 0; // loop counter
+    while (i < N) free(P[i++]); // free each row pointer
 }
