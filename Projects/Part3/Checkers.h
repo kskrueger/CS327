2e47854a22197d15900e35996440e044ace24dac
Karter Krueger <karterk@iastate.edu>
2019-11-14 21:37:05 -0600

    Part3

--- Projects/Part3/Checkers.h
+++ Projects/Part3/Checkers.h
@@ -665,8 +665,8 @@ pointLoc *getPoints(char myBoard[8][8], int turn) {
     char c = turn ? 'r' : 'b';
     int num = 0;
     pointLoc *points = calloc(count(myBoard, (char)tolower(c)) + count(myBoard, (char)toupper(c)) + 1, sizeof(pointLoc));
-    for (int i = 7; i >= 0; i--)
-        for (int j = 0; j < 8; j++) {
+    for (int j = 0; j < 8; j++) {
+        for (int i = 7; i >= 0; i--) {
             if (myBoard[i][j] == tolower(c) || myBoard[i][j] == toupper(c)) {
                 points[num] = malloc(sizeof(struct sim_point));
                 points[num]->r = i;
@@ -675,6 +675,7 @@ pointLoc *getPoints(char myBoard[8][8], int turn) {
                 //points = realloc(points, num*sizeof(struct sim_point));
             }
         }
+    }
     points[num] = NULL;
     return points;
 }
@@ -918,6 +919,7 @@ pointMove *getMoves_m(char myBoard[8][8], pointLoc point) {
     char high = (char)toupper(c);
     char jump = (low == 'r') ? 'b' : 'r';
     int y = (low == 'r') ? -1 : 1;
+    int x = (low == 'r') ? 1 : -1;
     int move_count = 0;
 
     if (myBoard[i+y][j-1] == '.') movesList[move_count++] = new_move(i, j, i+y, j-1);
